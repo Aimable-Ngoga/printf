033197bc397365770af99992e2c0f386ccba25dc
SamKinnon <garysamjones@gmail.com>
2023-08-22 17:24:03 +0200

    Main file of the project

--- _printf.c
+++ _printf.c
@@ -1,5 +1,5 @@
 #include "main.h"
-
+/** ++++++++function declaration++++ */
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
@@ -9,7 +9,7 @@ void print_buffer(char buffer[], int *buff_ind);
  */
 int _printf(const char *format, ...)
 {
-int i, printed = 0, printed_chars = 0;
+	int i, printed = 0, printed_chars = 0;
 	int flags, width, precision, size, buff_ind = 0;
 	va_list list;
 	char buffer[BUFF_SIZE];
@@ -26,7 +26,6 @@ int i, printed = 0, printed_chars = 0;
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -53,8 +52,9 @@ int i, printed = 0, printed_chars = 0;
 }
 
 /**
- * print_buffer - Prints the contents of the buffer if it exist
- * @buffer: Array of chars
+ * print_buffer - function Prints the contents of the buffer 
+ * if it exist
+ * @buffer: Array of characts
  * @buff_ind: Index at which to add next char, represents the length.
  */
 void print_buffer(char buffer[], int *buff_ind)
